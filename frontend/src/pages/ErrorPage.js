b1db3614e8fcfef9fbb94979f7ca8716c2a6e460
Nischal Bista <nischalbista0@gmail.com>
2024-01-05 13:49:00 +0545

    Change styling in error page

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -2,14 +2,17 @@ import React from "react";
 
 const ErrorPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-screen">
       <div className="text-center">
-        <h1 className="text-4xl font-bold text-red-600 mb-4">Error</h1>
-        <p className="text-gray-600 mb-8">
+        <img
+          src="https://cdn.dribbble.com/users/761395/screenshots/6287961/error_401.jpg?resize=400x0"
+          alt=""
+        />
+        <p className="text-black-75 font-medium mb-4">
           Oops! It seems like you need to log in to access this feature.
         </p>
         <a
-          href="/signin"  
+          href="/signin"
           className="text-white text-lg font-medium bg-purple-lighter hover:bg-purple-lighter-hover py-2 px-8 rounded-lg cursor-pointer transition duration-300"
         >
           Log In
